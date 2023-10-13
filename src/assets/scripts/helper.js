b307f6107dbbcf097cf47ab376104b33adb231b7
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2023-10-13 12:17:17 +0600

    save changes

--- src/assets/scripts/helper.js
+++ src/assets/scripts/helper.js
@@ -3,20 +3,74 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-export function swiperInit({ className, perView, spaceBetween, nextBtnClass, prevBtnClass, direction }) {
-  const slidesPerView = perView || 1
+export function swiperInit({ className, perView, spaceBetween, nextBtnClass, prevBtnClass, direction }, sizes) {
+  const wSize = getWindowSize()
+  let slidesPerView = perView || (sizes && sizes[wSize]?.perView) || 1
   const space = spaceBetween
 
-  new Swiper(className, {
-    direction: direction || 'horizontal',
-    modules: [Navigation],
-    slidesPerView: slidesPerView,
-    spaceBetween: space,
-    navigation: {
-      nextEl: nextBtnClass,
-      prevEl: prevBtnClass,
-    },
-  });
+  let swiper = getSwiper()
+
+  function getSwiper() {
+    let swiper = new Swiper(className, {
+      direction: direction || 'horizontal',
+      modules: [Navigation],
+      slidesPerView: slidesPerView,
+      spaceBetween: space,
+      loop: true,
+      navigation: {
+        nextEl: nextBtnClass,
+        prevEl: prevBtnClass,
+      },
+    });
+
+    let initedSize;
+
+    swiper.on('resize', () => {
+      if (!sizes) return
+  
+      const windowSize = getWindowSize()
+  
+      if (windowSize === initedSize) return
+  
+      initedSize = windowSize
+  
+      slidesPerView = sizes[initedSize]?.perView || 1
+      swiper.destroy()
+      swiper = getSwiper(slidesPerView)
+    })
+  
+    slideChange(swiper)
+    
+    swiper.on('slideChangeTransitionEnd', slideChange)
+
+    return swiper
+  }
+
+  function slideChange(sw) {
+    if (slidesPerView === 1) return
+
+    const activeEl = sw.slides.find((e) => e.classList.contains('swiper-slide-active'))
+    if (activeEl) {
+      if (activeEl.previousElementSibling) activeEl.previousElementSibling.style.opacity = 0;
+      
+      const elemOpacFunc = swiperHide(slidesPerView)
+
+      elemOpacFunc(activeEl)
+    }
+  }
+
+  function swiperHide(count) {
+    let c = 0
+
+    function rec(el) {
+      c += 1
+      if (c > count) return
+      el.style.opacity = 1;
+      if (el.nextElementSibling) rec(el.nextElementSibling)
+    }
+
+    return rec
+  }
 }
 
 export function getWindowSize() {
@@ -28,24 +82,5 @@ export function getWindowSize() {
 }
 
 export function adaptiveSwiper(opt, sizes) {
-  let initedSize;
-
-  const init = () => {
-    setTimeout(() => {
-      const windowSize = getWindowSize()
-
-      if (windowSize === initedSize) return
-
-      initedSize = windowSize
-
-      swiperInit({
-        ...opt,
-        perView: sizes[initedSize]?.perView || opt.perView || 2,
-      })
-    }, 200)
-  }
-
-  init()
-
-  window.addEventListener('resize', init)
+  swiperInit(opt, sizes)
 }
